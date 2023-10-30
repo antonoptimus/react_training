97400475f7c3e072eca181eefb31c2c14e68dcef
Anton Eliseev <anton_saintp@mail.ru>
2023-10-30 13:07:51 +0300

    css modules, components, ui lib, children

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
-import Counter from "./components/counter";
+import "./styles/App.css";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-  const [value, setValue] = useState("TEXT IN INPUT");
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript 1", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
+  const [title, setTitle] = useState("");
+
+  const addNewPost = () => {};
 
   return (
     <div className="App">
-      <Counter />
+      <form>
+        <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"
+        />
+        <MyInput type="text" placeholder="Описание поста" />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Посты про JS" key={posts.id} />
     </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,24 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Counter = function () {
-   const [count, setCount] = useState(0);
- 
-   function increment() {
-     setCount(count + 1);
-   }
- 
-   function decrement() {
-     setCount(count - 1);
-   }
- 
-   return (
-     <div>
-       <h1>{count}</h1>
-       <button onClick={increment}>inc</button>
-       <button onClick={decrement}>dec</button>
-     </div>
-   );
- 
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>inc</button>
+      <button onClick={decrement}>dec</button>
+    </div>
+  );
 }
 
 export default Counter;
