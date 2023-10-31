d766179e0b70a620040ced2c24b383523b6266fc
Anton Eliseev <anton_saintp@mail.ru>
2023-10-31 17:06:30 +0300

    sorting

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -12,25 +13,46 @@ function App() {
     { id: 3, title: "Javascript 3", body: "Description" },
   ]);
   const [post, setPost] = useState({ title: "", body: "" });
-
-  const addNewPost = (e) => {
-    e.preventDefault();
-    setPosts([...posts, { ...post, id: Date.now() }]);
-    setPost({ title: "", body: "" });
-  };
+  const [selectedSort, setSelectedSort] = useState("");
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   };
 
   const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
   };
 
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList remove={removePost} posts={posts} title="Посты про JS" key={posts.id} />
+      <hr style={{ margin: "15px 0" }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
+          ]}
+        />
+      </div>
+      {posts.length !== 0 ? (
+        <PostList
+          remove={removePost}
+          posts={posts}
+          title="Посты про JS"
+          key={posts.id}
+        />
+      ) : (
+        <h1 style={{ textAlign: "center " }}>Посты не найдены</h1>
+      )}
     </div>
   );
 }
